5fc7d9c20fce34869de266e69d7178ace3218e51
Russell Oakham <russell.oakham@and.digital>
2024-04-05 21:30:40 +0100

    middelware - adapted parsing middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-import { AnyZodObject } from "zod";
+import { z, AnyZodObject } from "zod";
+
+// const validate =
+//   (schema: AnyZodObject) =>
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       await schema.parseAsync({
+//         body: req.body,
+//         query: req.query,
+//         params: req.params,
+//       });
+//       return next();
+//     } catch (error) {
+//       return res.status(400).json(error);
+//     }
+//   };
 
 const validate =
-  (schema: AnyZodObject) =>
+  <T extends z.ZodTypeAny>(schema: T) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       await schema.parseAsync({
@@ -15,3 +30,15 @@ const validate =
       return res.status(400).json(error);
     }
   };
+
+const parse = <T extends z.ZodTypeAny>(
+  schema: T,
+  data: unknown
+): z.infer<T> => {
+  try {
+    return schema.parse(data);
+  } catch (error: unknown) {
+    const e = error as z.ZodError;
+    throw new Error(`Parsing Error: ${JSON.stringify(e.errors)}`);
+  }
+};
